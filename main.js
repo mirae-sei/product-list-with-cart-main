1673dd399a0c34f92f9c4b48e40935efb52e580c
sei <blessdugan@gmail.com>
2024-11-10 17:06:09 +0800

    successfully fetch and display data from products.json

--- main.js
+++ main.js
@@ -1,78 +1,46 @@
-let productsHTML;
-let products = ['adasd'];
+let productsHTML = '';
+let products = [];
 
 let productContainer = document.querySelector('.flex-box-container');
 
-
-
-
-
-function fetchAndDisplay(){
-
-
-fetch('data.json').then(response =>{
-  if(!response.ok){
-    throw new Error('NOT OK');
-
-  }
-  return response.json();
+function fetchAndDisplay() {
+  fetch('data.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('NOT OK');
+      }
+      return response.json();
+    })
+    .then(data => {
+      data.forEach(item => {
+        products.push(item);
+      
+      });
+      console.log(products);
+      products.forEach((product) => {
+        productsHTML += `<div class="product-container">
+          <div class="image-and-button">
+            <img src="${product.image.desktop}">
+            <button> 
+              <img src="./assets/images/icon-add-to-cart.svg"> Add to Cart
+            </button>
+          </div>
+          <div class="product-info">
+            <p class="product-category">${product.category}</p>
+            <h3 class="product-name">${product.name}</h3>
+            <h3 class="price">$${product.price.toFixed(2)}</h3>
+          </div>
+        </div>`;
+      
+        productContainer.innerHTML = productsHTML;
+      });
+      
+      
+      console.log(productsHTML);
+    })
+    .catch(error => {
+      console.log(error);
+    });
 }
- ).then(
-  data => {
-   data.forEach(item => {
-    products.push(item);
-    console,log
-   });
-
-
-
-
-  products.forEach((product) => {
-    productsHTML += `<div class="product-container">
-      <div class="image-and-button">
-        <img src="${product.image.mobile}">
-        <button> Add to Cart</button>
-      </div>
-      <div class="product-info">
-        <p class="product-category">${product.category}</p>
-        <h3 class="product-name">${product.name}</h3>
-        <h3>$${product.price}</h3>
-      </div>
-    </div>`;
-    
-
-  })
-   
-  }
- ).catch(error => {
-  console.log(error);
- }
- )
-
- 
-}
-
 
 fetchAndDisplay();
-console.log(products);
-
-
-
-
- 
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
